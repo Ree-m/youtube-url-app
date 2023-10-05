06511e966ad1e6c4c9e128a155da559bf5e073b7
Ree-m <reembsrat@gmail.com>
2023-10-05 07:54:27 +0300

    added loading component and test route

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,10 +1,7 @@
 
-// exports.getTest=async(req,res)=>{
-//     console.log("test")
-//     return res.json("test")
-//   }
+exports.getTest=async(req,res)=>{
+    console.log("test")
+    return res.json("test")
+  }
 
-exports.getTest=(a,b)=>{
-  return a+b
-}
 
